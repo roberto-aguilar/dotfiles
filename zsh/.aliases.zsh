0d06938f77506d62254f15fc24da856bfe1214bf
Roberto Aguilar <raguilar@dojogeek.io>
2017-02-04 21:28:33 -0600

    Create and load personal aliases, functions and third party commands

---
+++ zsh/.aliases.zsh
@@ -0,0 +1,12 @@
+# Reload the zsh settings.
+alias reload="source ~/.zshrc"
+
+# Nuke the shit out of the target file or directory
+alias nuke="rm -rf"
+
+# Update the composer autoloader
+alias dal="composer dumpautoload"
+
+# Handy alias to run phpunit
+alias u="vendor/bin/phpunit"
+
---
+++ zsh/.third-party.zsh
@@ -0,0 +1,9 @@
+# File executed before the python's repl runs
+export PYTHONSTARTUP=$HOME/.pythonrc
+
+# Python's virtualenvwrapper
+source /usr/local/bin/virtualenvwrapper.sh
+
+# Z command
+source ~/Bin/z.sh
+
