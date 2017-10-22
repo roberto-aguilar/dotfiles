c6dc33de9048cab8ea1b1eb5c18d3aadd6f2c587
Roberto Aguilar <raguilar@dojogeek.io>
2017-10-21 20:23:33 -0500

    Remove z and virtualenvwrapper

--- zsh/.third-party.zsh
+++ zsh/.third-party.zsh
@@ -1,8 +1,3 @@
 # File executed before the python's repl runs
 export PYTHONSTARTUP=$HOME/.pythonrc
 
-# Python's virtualenvwrapper
-source /usr/local/bin/virtualenvwrapper.sh
-
-# Z command
-source ~/Bin/z.sh
