3533f93ca6544cb3e8bfbf3a70892c27ea4d911c
Roberto Aguilar <roberto.aguilar.arrieta@gmail.com>
2021-10-27 07:04:00 -0500

    Remove no longer used aliases

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -3,11 +3,3 @@ alias reload="source ~/.zshrc"
 
 # Nuke the shit out of the target file or directory
 alias nuke="rm -rf"
-
-# Update the composer autoloader
-alias dal="composer dumpautoload"
-
-# Phpunit
-alias t="phpunit"
-alias tf="phpunit --filter ${1}"
-
