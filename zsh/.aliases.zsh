e0037ecf5c1c6c1f15dcc54bf2af10087ee0f018
Roberto Aguilar <roberto.aguilar.arrieta@gmail.com>
2025-08-19 18:01:52 -0600

    Define handy PHP development aliases

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -3,3 +3,9 @@ alias reload="source ~/.zshrc"
 
 # Nuke the shit out of the target file or directory
 alias nuke="rm -rf"
+
+# Handy PHP development aliases.
+alias pest="vendor/bin/pest"
+alias phpunit="vendor/bin/phpunit"
+alias pint="vendor/bin/pint"
+alias sail="vendor/bin/sail"
