586510dff67bdc67e5438d596c1409e768cdd236
Roberto Aguilar <roberto.aguilar.arrieta@gmail.com>
2025-09-08 13:55:22 -0600

    Override `cd` with my own function

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -4,6 +4,9 @@ alias reload="source ~/.zshrc"
 # Nuke the shit out of the target file or directory
 alias nuke="rm -rf"
 
+# Override `cd` command with my own function.
+alias cd="zd"
+
 # Handy PHP development aliases.
 alias pest="vendor/bin/pest"
 alias phpunit="vendor/bin/phpunit"
--- zsh/.functions.zsh
+++ zsh/.functions.zsh
@@ -24,3 +24,14 @@ n() {
     nvim "$@"
   fi
 }
+
+# Improvements over zoxide's z command.
+zd() {
+  if [ $# -eq 0 ]; then
+    builtin cd ~ && return
+  elif [ -d "$1" ]; then
+    builtin cd "$1"
+  else
+    z "$@" && printf "\U000F17A9 " && pwd || echo "Error: Directory not found"
+  fi
+}
