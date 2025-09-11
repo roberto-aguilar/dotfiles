82d36c2383211017b2d447b5172144e0c37777b2
Roberto Aguilar <roberto.aguilar.arrieta@gmail.com>
2025-09-10 20:36:54 -0600

    Install and initialize `mise` command

--- zsh/.initialize.zsh
+++ zsh/.initialize.zsh
@@ -7,3 +7,8 @@ fi
 if command -v zoxide &> /dev/null; then
     eval "$(zoxide init zsh)"
 fi
+
+# Initialize `mise` if available.
+if command -v mise &> /dev/null; then
+  eval "$(mise activate zsh)"
+fi
