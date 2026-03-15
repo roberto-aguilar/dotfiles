cc6c228c1f45e6adc5d1294c4a43ca3e1ed18405
Roberto Aguilar <roberto.aguilar.arrieta@gmail.com>
2026-03-15 09:18:02 -0600

    Add NeoVim config managed via stow

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+          },
+        },
+      },
+    },
+  },
+}
