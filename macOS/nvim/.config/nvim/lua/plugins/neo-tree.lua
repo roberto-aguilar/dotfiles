ed970c607c58d8a4f29f2e2563fb8c54438fc964
Roberto Aguilar <roberto.aguilar.arrieta@gmail.com>
2026-04-01 09:44:22 -0600

    Strip git and diagnostic clutter from `neo-tree`
    The git status and LSP diagnostic icons in neo-tree add visual noise
    without much value.
    
    - Disable async git status fetching
    - Remove git status icons from the filesystem view
    - Remove diagnostic icons from the filesystem view

--- macOS/nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ macOS/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -2,10 +2,24 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      git_status_async = false,
+      default_component_configs = {
+        git_status = {
+          symbols = {},
+        },
+      },
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
           hide_gitignored = false,
+        },
+        components = {
+          git_status = function()
+            return {}
+          end,
+          diagnostics = function()
+            return {}
+          end,
         },
       },
     },
